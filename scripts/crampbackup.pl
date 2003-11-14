b96c9039c64b58031356743b7e53d7cc3b7d42a6
Dhruva Krishnamurthy <dhruvakm@gmail.com>
2003-11-14 06:09:09 +0000

    Changes to use TAR abd BZIP2

--- scripts/crampbackup.pl
+++ scripts/crampbackup.pl
@@ -1,27 +1,35 @@
 #!perl
+# Time-stamp: <2003-11-14 11:36:14 dhruva>
 ##-----------------------------------------------------------------------------
 ## File: crampbackup.pl
 ## Desc: Perl script to backup the CRAMP repository
 ##-----------------------------------------------------------------------------
 ## mm-dd-yyyy  History                                                     user
 ## 10-07-2003  Cre                                                          dky
+## 11-14-2003  Mod  Use BZIP2 and TAR                                       dky
 ##-----------------------------------------------------------------------------
-$source="E:/CRAMP/CVSREPOSITORY";
-$target="E:/BACKUPS/cramp";
+my $BASEDRIVE='d';
+if(exists($ENV{'BASEDRIVE'})){
+    $BASEDRIVE=$ENV{'BASEDRIVE'};
+}
+$source="$BASEDRIVE:/CRAMP/CVSREPOSITORY";
+$target="$BASEDRIVE:/BACKUPS/cramp";
+
 die("Unable to find source or target") unless(-d $source && -d $target);
 my @curr=localtime();
 $year=$curr[5]+1900;
 $curr[4]++;
-$name="$target/CRAMP_$curr[2]_$curr[1]#$curr[4]_$curr[3]_$year.zip";
+$name="$target/CRAMP_$curr[2]_$curr[1]#$curr[4]_$curr[3]_$year.tar";
 
 while(-f $name){
     sleep(60-$curr[0]);
     @curr=localtime();
     $year=$curr[5]+1900;
-    $name="$target/CRAMP_$curr[2]_$curr[1]#$curr[4]_$curr[3]_$year.zip";
+    $name="$target/CRAMP_$curr[2]_$curr[1]#$curr[4]_$curr[3]_$year.tar";
 }
 
-open(ZIP,"zip.exe -9 -r $name $source|")
-    || die("Cannot ZIP $source to $name");
-close(ZIP);
-print("Zipped file: $name\n");
+system("tar.exe --force-local -cf $name -C $source cramp")==0
+    || die("Failed in TAR");
+system("bzip2.exe --force $name")==0
+    || die("Failed in BZIP2");
+print("BZIP file: $name.bz2\n");
