73f8c94cc5d65491df6c43a834a743260d80fa35
Dhruva Krishnamurthy <dhruvakm@gmail.com>
2004-02-25 11:57:53 +0000

    Can give a comma seperated list of PID

--- utils/ProfileControl.cpp
+++ utils/ProfileControl.cpp
@@ -1,5 +1,5 @@
 // -*-c++-*-
-// Time-stamp: <2003-12-03 10:44:40 dhruva>
+// Time-stamp: <2004-02-25 12:55:29 dky>
 //-----------------------------------------------------------------------------
 // File  : ProfileControl.cpp
 // Usage : ProfileControl.exe PROFILE_HOST PID START|STOP|FLUSH
@@ -7,6 +7,7 @@
 //-----------------------------------------------------------------------------
 // mm-dd-yyyy  History                                                      tri
 // 12-03-2003  Cre                                                          dky
+// 02-04-2004  Mod  Can give list of PID (comma separated) to control       dky
 //-----------------------------------------------------------------------------
 #define __PROFILECONTROL_SRC
 
@@ -22,47 +23,58 @@ WINAPI WinMain(HINSTANCE hinstExe,
                HINSTANCE,
                PSTR pszCmdLine,
                int nCmdShow){
-  // Get the command line stuff
-  int argcW=0;
-  LPWSTR *argvW=0;
-  argvW=CommandLineToArgvW(GetCommandLineW(),&argcW);
-  if(!argvW)
-    return(-1);
-  if(argcW<4){
-    GlobalFree(argvW);
-    return(-1);
-  }
+    // Get the command line stuff
+    int argcW=0;
+    LPWSTR *argvW=0;
+    argvW=CommandLineToArgvW(GetCommandLineW(),&argcW);
+    if(!argvW)
+        return(-1);
+    if(argcW<4){
+        GlobalFree(argvW);
+        return(-1);
+    }
 
-  // Currently supports only 1 arg
-  char *buff=0;
-  char pid[256];
-  char msg[256];
-  char comp[256];
-  char mspath[MAX_PATH];
-  WideCharToMultiByte(CP_ACP,0,argvW[1],-1,
-                      comp,256,0,0);
-  WideCharToMultiByte(CP_ACP,0,argvW[2],-1,
-                      pid,256,0,0);
-  WideCharToMultiByte(CP_ACP,0,argvW[3],-1,
-                      msg,256,0,0);
-  sprintf(mspath,"\\\\%s\\mailslot\\cramp_mailslot#%s",comp,pid);
+    // Currently supports only 1 arg
+    int ret=0;
+    char *buff=0;
+    char *pid=0;
+    char msg[256];
+    char comp[256];
+    char mspath[MAX_PATH];
+    char pidlist[MAX_PATH];
+    HANDLE h_file=0;
 
-  HANDLE h_file=0;
-  h_file=CreateFile(mspath,
-                    GENERIC_WRITE,
-                    FILE_SHARE_READ,
-                    NULL,
-                    OPEN_EXISTING,
-                    FILE_ATTRIBUTE_NORMAL,
-                    NULL);
-  if(h_file==INVALID_HANDLE_VALUE)
-    return(-1);
+    WideCharToMultiByte(CP_ACP,0,argvW[1],-1,
+                        comp,256,0,0);
+    WideCharToMultiByte(CP_ACP,0,argvW[2],-1,
+                        pidlist,MAX_PATH,0,0);
+    WideCharToMultiByte(CP_ACP,0,argvW[3],-1,
+                        msg,256,0,0);
 
-  DWORD cbWritten=0;
-  int ret=0;
-  ret=WriteFile(h_file,msg,strlen(msg)+1,&cbWritten,NULL);
-  CloseHandle(h_file);
-  h_file=0;
+    // Loop through all PID's
+    pid=strtok(pidlist,",");
+    while(pid){
+        sprintf(mspath,"\\\\%s\\mailslot\\cramp_mailslot#%s",comp,pid);
+        pid=strtok(NULL,",");
 
-  return(!ret);
+        h_file=CreateFile(mspath,
+                          GENERIC_WRITE,
+                          FILE_SHARE_READ,
+                          NULL,
+                          OPEN_EXISTING,
+                          FILE_ATTRIBUTE_NORMAL,
+                          NULL);
+        if(h_file==INVALID_HANDLE_VALUE){
+            ret=1;
+            continue;
+        }
+
+        DWORD cbWritten=0;
+        if(!WriteFile(h_file,msg,strlen(msg)+1,&cbWritten,NULL))
+            ret=1;
+
+        CloseHandle(h_file);
+        h_file=0;
+    }
+    return(ret);
 }
